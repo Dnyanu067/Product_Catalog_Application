a8fc0fcbe5e4d792a2d798f29d05420d244e6287
Dnyanu067 <dnyaneshwarwagh067@gmail.com>
2025-04-04 17:40:12 +0530

    first

---
+++ src/main/java/com/ProductCatalog/ProductCatalogApplication.java
@@ -0,0 +1,14 @@
+package com.ProductCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogApplication.class, args);
+		System.out.println("run successfully");
+	}
+
+}
